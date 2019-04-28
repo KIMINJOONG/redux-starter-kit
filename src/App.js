eca6aed1c87dba77660a866ed3f94b7d4a200867
KIMINJOONG <ijboym216@gmail.com>
2019-04-28 12:08:15 +0900

    use axios redux-thunk

--- src/App.js
+++ src/App.js
@@ -2,14 +2,38 @@ import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as counterActions from "./modules/counter";
+import post, * as postActions from "./modules/post";
 
 class App extends Component {
+  loadData = () => {
+    const { PostActions, number } = this.props;
+    PostActions.getPost(number);
+  };
+  componentDidMount() {
+    this.loadData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.number !== prevProps.number) {
+      this.loadData();
+    }
+  }
   render() {
-    const { CounterActions, number } = this.props;
+    const { CounterActions, number, post, error, loading } = this.props;
 
     return (
       <div>
         <h1>{number}</h1>
+        {loading ? (
+          <h2>로딩중</h2>
+        ) : error ? (
+          <h2>오류발생</h2>
+        ) : (
+          <div>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        )}
         <button onClick={CounterActions.incrementAsync}>+</button>
         <button onClick={CounterActions.decrementAsync}>-</button>
       </div>
@@ -19,9 +43,13 @@ class App extends Component {
 
 export default connect(
   state => ({
-    number: state.counter
+    number: state.counter,
+    post: state.post.data,
+    loading: state.post.pending,
+    error: state.post.error
   }),
   dispatch => ({
-    CounterActions: bindActionCreators(counterActions, dispatch)
+    CounterActions: bindActionCreators(counterActions, dispatch),
+    PostActions: bindActionCreators(postActions, dispatch)
   })
 )(App);
